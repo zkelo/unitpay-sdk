f761e7e02b73f065c98a81a0ef9b79ce20de9fb1
Aleksandr Riabov <ar161ru@gmail.com>
2021-03-20 15:53:31 +0300

    Updated LocaleInterface

--- src/Interfaces/LocaleInterface.php
+++ src/Interfaces/LocaleInterface.php
@@ -23,27 +23,4 @@ interface LocaleInterface
      * @return array
      */
     public static function rawMessages(): array;
-
-    /**
-     * Checks if message with specified key exists
-     *
-     * @param string $key Message key
-     * @return boolean `true` if message found or `false` if not
-     */
-    public static function hasMessage(string $key): bool;
-
-    /**
-     * Returns localized message
-     *
-     * @param string $key Message key
-     * @return string|null Message or `null` if key not found
-     */
-    public static function message(string $key): ?string;
-
-    /**
-     * Alias of `message()`
-     *
-     * @see static::message
-     */
-    public static function t(string $key): ?string;
 }
